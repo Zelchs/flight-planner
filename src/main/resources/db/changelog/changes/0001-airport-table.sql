dce0f06769d6137f28baeefa426554c070b87b1e
Zelchs <raivis0211@gmail.com>
2024-05-29 13:34:57 +0300

    Modify airport table

--- src/main/resources/db/changelog/changes/0001-airport-table.sql
+++ src/main/resources/db/changelog/changes/0001-airport-table.sql
@@ -7,7 +7,7 @@ CREATE TABLE airport
     id      INT AUTO_INCREMENT PRIMARY KEY,
     country VARCHAR(255) NOT NULL,
     city    VARCHAR(255) NOT NULL,
-    airport VARCHAR(255) NOT NULL UNIQUE
+    airport VARCHAR(255) NOT NULL
 );
 
 
